b70a0f073f930a3c65642e05ebd4bab8794d7c42
Jose M. Torres <jmtorresr@gmail.com>
2020-10-19 19:42:48 +0900

    Created database and successfully connected to the data

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+const knex = require('knex')({
+    client: 'postgres',
+    connection: {
+      host : '127.0.0.1',
+      user : '3301hakuba',
+      password : 'your_database_password',
+      database : 'hakuba_hotsprings'
+    }
+  });
+
+const names = knex
+.select()
+.table("name")
+.then((rows) => {
+    console.log(rows)
+});
+
 const express = require('express');
 const app = express();
 const port = 3000;
