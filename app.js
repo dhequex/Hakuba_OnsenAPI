2316bf095a4280193bd8d09fa3e1f00e6a6cb845
Jose M. Torres <jmtorresr@gmail.com>
2020-10-19 22:27:59 +0900

    Removed .env variables due to error proces.env.DB_USER does not exist

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
 const knexconfig = require("./knexfile");
 const knex = require('knex')(knexconfig);
 
-const names = knex
-.select()
-.table("name")
-.then((rows) => {
-    console.log(rows)
-});
-
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.use(express.static("/"));
 
-app.get("/api/", (req, res) => {
+app.get("/api/name", (req, res) => {
     knex
     .select()
     .from('name')
+    .then ((data) => {
+        console.log(data)
+        res.send(data);
+    });
+});
+
+app.get("/api/facilities", (req, res) => {
+    knex
+    .select()
+    .from('facilities')
+    .then ((data) => {
+        console.log(data)
+        res.send(data);
+    });
+});
+
+app.get("/api/businesshours", (req, res) => {
+    knex
+    .select()
+    .from('BusinessHours')
+    .then ((data) => {
+        console.log(data)
+        res.send(data);
+    });
+});
+
+app.get("/api/businesshours", (req, res) => {
+    knex
+    .select()
+    .from('BusinessHours')
     .then ((data) => {
         console.log(data)
         res.send(data);
--- knexfile.js
+++ knexfile.js
@@ -1,46 +1,16 @@
-require("dotenv").config();
+//require("dotenv").config({path: '/'});
 // Update with your config settings.
 
 module.exports = {
 
-  development: {
-      client:'pg',
+  client: 'pg',
+
       connection: {
         host : '127.0.0.1',
-        user : 'process.env.DB_USER',
-        database : 'process.env.DB_NAME'
+        user : '3301hakuba',
+        database : 'hakuba_hotsprings'
       }
     
-  },
+  };
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'hakuba_hotsprings',
-      user:     '3301hakuba',
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'hakuba_hotsprings',
-      user:     '3301hakuba',
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
-};
